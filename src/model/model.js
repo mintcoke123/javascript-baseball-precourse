d26b3db55cc3cc548220db1c85e5be2c6e731f02
mintcoke123 <weetee1014@gmail.com>
2025-03-24 23:24:51 +0900

    refactor: Model 캡슐화

--- src/model/model.js
+++ src/model/model.js
@@ -1,33 +1,41 @@
 export default class BaseballGameModel {
+  #computerInputNumbers;
+  #userInputNumbers;
+  #strike;
+  #ball;
+
   constructor() {
-    this.computerInputNumbers = "";
-    this.userInputNumbers = "";
-    this.strike = 0;
-    this.ball = 0;
+    this.#computerInputNumbers = "";
+    this.#userInputNumbers = "";
+    this.#strike = 0;
+    this.#ball = 0;
   }
 
   play() {
-    this.calculateStrikeBall(this.computerInputNumbers, this.userInputNumbers);
-    return this.generateStrikeBallMessage();
+    this.#calculateStrikeBall();
+    return this.#generateStrikeBallMessage();
   }
 
-  calculateStrikeBall() {
-    this.ball = 0;
-    this.strike = 0;
+  #calculateStrikeBall() {
+    this.#ball = 0;
+    this.#strike = 0;
 
     for (let index = 0; index < 3; index += 1) {
-      if (this.computerInputNumbers[index] === this.userInputNumbers[index])
-        this.strike += 1;
-      else if (this.computerInputNumbers.includes(this.userInputNumbers[index]))
-        this.ball += 1;
+      if (this.#computerInputNumbers[index] === this.#userInputNumbers[index]) {
+        this.#strike += 1;
+      } else if (
+        this.#computerInputNumbers.includes(this.#userInputNumbers[index])
+      ) {
+        this.#ball += 1;
+      }
     }
   }
 
-  generateStrikeBallMessage() {
-    if (this.strike === 0 && this.ball === 0) return "낫싱";
-    if (this.strike === 0) return `${this.ball}볼`;
-    if (this.ball === 0) return `${this.strike}스트라이크`;
-    return `${this.ball}볼 ${this.strike}스트라이크`;
+  #generateStrikeBallMessage() {
+    if (this.#strike === 0 && this.#ball === 0) return "낫싱";
+    if (this.#strike === 0) return `${this.#ball}볼`;
+    if (this.#ball === 0) return `${this.#strike}스트라이크`;
+    return `${this.#ball}볼 ${this.#strike}스트라이크`;
   }
 
   generateRandomNumberString() {
@@ -36,11 +44,11 @@ export default class BaseballGameModel {
       const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
       numbers.add(randomNumber);
     }
-    this.computerInputNumbers = [...numbers].join("");
+    this.#computerInputNumbers = [...numbers].join("");
   }
 
   userInput(input) {
-    this.userInputNumbers = input;
+    this.#userInputNumbers = input;
   }
 
   isValidInput() {
@@ -50,17 +58,17 @@ export default class BaseballGameModel {
   }
 
   isNumbersBetweenOneAndNine() {
-    return [...this.userInputNumbers].every(
+    return [...this.#userInputNumbers].every(
       (element) => element >= "1" && element <= "9"
     );
   }
 
   isDifferent() {
-    const uniqueNumber = [...new Set(this.userInputNumbers)];
+    const uniqueNumber = [...new Set(this.#userInputNumbers)];
     return uniqueNumber.length === 3;
   }
 
   isThree() {
-    return this.userInputNumbers.length === 3;
+    return this.#userInputNumbers.length === 3;
   }
 }
